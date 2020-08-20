c5ef6f73166944d903ab38cd2d3adc85b427c242
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-20 19:46:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const createEmployeeRecord = info => {
+  const record = {
+    firstName: info[0],
+    familyName: info[1],
+    title: info[2],
+    payPerHour: info[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return record;
+};
+
+const CreateEmployeeRecords = array => array.map( employee => CreateEmployeeRecords(employee))
